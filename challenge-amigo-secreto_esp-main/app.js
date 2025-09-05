a8faab70832b0a32c2e9a1e943285d6ea2181cf7
Jose Eduardo Ramos Martinez <141459860+JRM01@users.noreply.github.com>
2025-09-05 02:29:31 -0600

    Actualizacion app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos  = [];
+
+function agregarAmigo(nombre){
+    let amigo = String(document.getElementById("amigo").value);
+    if(amigo == ""){
+        alert("Por favor ingresa un nombre válido");
+
+    } else{
+    nombresAmigos.push(amigo);
+    document.getElementById("amigo").value = "";
+    mostraramigos();
+    }
+}
+
+function mostraramigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for(let i = 0; i < nombresAmigos.length; i++){
+        let li = document.createElement("li");
+        li.innerText = nombresAmigos[i];
+        lista.appendChild(li);
+    }
+}
+function sortearAmigo(){
+    if(nombresAmigos.length ==0){
+        alert("Por favor ingresa al menos un nombre");
+    }
+    else{
+        let amigo = nombresAmigos[Math.floor(Math.random() * nombresAmigos.length)];
+        document.getElementById("resultado").innerText = "El amigo secreto es: " + amigo;
+    }
+}
